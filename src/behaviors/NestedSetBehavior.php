0119c534f0aa0422d7f703e0fb47c37cdb17dade
Dmitriy Brusenskiy <brusenskiydmitriy@gmail.com>
2015-12-12 10:03:02 +0300

    Add Nested sets behavior

---
+++ src/behaviors/NestedSetBehavior.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Horex CMS
+ * @author Brusenskiy Dmitry <brussens@horex.xyz>
+ * @since 1.0.0
+ * @version 1.0.0
+ * @link http://horex.xyz <Official site>
+ * @copyright 2015 Brusenskiy Dmitry
+ * @license http://opensource.org/licenses/Apache-2.0 Apache 2.0
+ * @package brussens\modules\catalog\behaviors
+ */
+
+namespace brussens\modules\catalog\behaviors;
+
+
+class NestedSetBehavior
+{
+
+}
